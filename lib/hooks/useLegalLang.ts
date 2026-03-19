ed1744f3e3ab91fbd77e0ba619e14fbbbb7cb7df
unknown <nationpathindia@gmail.com>
2026-03-19 18:06:21 +0530

    fix legal lang error

--- lib/hooks/useLegalLang.ts
+++ lib/hooks/useLegalLang.ts
@@ -1,12 +1,3 @@
-import { useContext } from "react"
-import { LegalLangContext } from "@/app/(legal)/layout"
-
 export function useLegalLang() {
-  const context = useContext(LegalLangContext)
-
-  if (!context) {
-    throw new Error("useLegalLang must be used within LegalLayout")
-  }
-
-  return context
+  return "en"
 }
